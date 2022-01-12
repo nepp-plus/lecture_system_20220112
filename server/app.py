579296ca471897384bce3ca5525cb256e6a0abcb
nepp-plus <nepp.plus@gmail.com>
2022-01-12 17:14:53 +0900

    /user - PUT 회원가입 기능추가

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, request
 
 from server.db_connector import DBConnector
 
-from .api.user import login
+from .api.user import login, sign_up
 from .api.lecture import lecture_test
 
 # DB 연결 정보를 관리하는 클래스 생성 => 객체를 변수에 담아두자.
@@ -19,6 +19,11 @@ def create_app():
         # args : 쿼리파라미터에 들어있는 데이터들
         # form : 폼데이터에 담겨있는 데이터들
         return login(request.form.to_dict())
+    
+    # 회원가입
+    @app.put("/user")
+    def user_put():
+        return sign_up(request.form.to_dict())
     
     @app.post("/lecture")
     def lecture_post():
