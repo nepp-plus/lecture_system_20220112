d120f6325b52de068b9c55c8b09c22e65c4754a6
nepp-plus <nepp.plus@gmail.com>
2022-01-12 16:07:50 +0900

    DB 호출
     - API파일에서 별도로 db 변수 생성

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,6 +1,18 @@
 # 로그인 / 회원가입 등, 사용자 정보 관련 기능 모아두는 모듈
+# DB 연결정보 보관 변수를 import
+from server.db_connector import DBConnector
+
+db = DBConnector()
 
 def test():
+    
+    # DB의 모든 users 조회 쿼리.
+    sql = "SELECT * FROM users"
+    db.cursor.execute(sql)
+    all_list = db.cursor.fetchall()
+    
+    print(all_list)
+    
     return {
         '임시': '임시'
     }
--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from server.db_connector import DBConnector
 from .api.user import test
 
 # DB 연결 정보를 관리하는 클래스 생성 => 객체를 변수에 담아두자.
-db = DBConnector()
+# db = DBConnector()
 
 def create_app():
     app = Flask(__name__)
