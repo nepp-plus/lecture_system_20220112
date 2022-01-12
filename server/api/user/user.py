3d5ce39105b908eaff550dceacb77cfe772f966a
nepp-plus <nepp.plus@gmail.com>
2022-01-12 16:22:29 +0900

    실제 사용자 목록을 응답으로.

--- server/api/user/user.py
+++ server/api/user/user.py
@@ -1,6 +1,7 @@
 # 로그인 / 회원가입 등, 사용자 정보 관련 기능 모아두는 모듈
 # DB 연결정보 보관 변수를 import
 from server.db_connector import DBConnector
+from server.model import Users
 
 db = DBConnector()
 
@@ -11,8 +12,15 @@ def test():
     db.cursor.execute(sql)
     all_list = db.cursor.fetchall()
     
-    print(all_list)
+    all_users = []
+    
+    for row in all_list:
+        # Users(row) : Users형태의 인스턴스 생성 => 함수들도 내장하고 있다.
+        # 인스턴스에게 곧바로 -> get_data_object() 실행 명령
+        # 해당 유져의 정보를 활용한 dict가 리턴됨
+        #  곧바로 목록의 append의 재료로 활용
+        all_users.append( Users(row).get_data_object() )
     
     return {
-        '임시': '임시'
+        'users': all_users
     }
