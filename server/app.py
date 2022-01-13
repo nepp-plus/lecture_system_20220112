02fea092ccaf195910a82d395b2354a15fe5172f
nepp-plus <nepp.plus@gmail.com>
2022-01-13 10:01:54 +0900

    db 변수를 app.py 에서 가져올 수 있게 세팅
     - API로직들을 create_app 함수 내에서 import 시점 변경

--- server/app.py
+++ server/app.py
@@ -4,14 +4,16 @@ from flask import Flask, request
 
 from server.db_connector import DBConnector
 
-from .api.user import login, sign_up, find_user_by_email
-from .api.lecture import lecture_test
-
 # DB 연결 정보를 관리하는 클래스 생성 => 객체를 변수에 담아두자.
-# db = DBConnector()
+db = DBConnector()
 
 def create_app():
     app = Flask(__name__)
+    
+    # API 로직 함수/클래스 들은, create_app 함수에서만 필요함.
+    # 함수 내부에서 import 실행. => 순환참조 회피. (정상 동작)
+    from .api.user import login, sign_up, find_user_by_email
+    from .api.lecture import get_all_lectures
     
     # 기본 로그인
     @app.post("/user")
