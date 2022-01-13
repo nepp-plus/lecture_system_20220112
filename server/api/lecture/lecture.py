e4af2bcbdb137da5d46a9af703f2ea4fc6e738bb
nepp-plus <nepp.plus@gmail.com>
2022-01-13 10:10:22 +0900

    모든 강의 목록 조회 기능 추가
     - 로직 작성 X, 단순 접속만 열어둠

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -1,5 +1,8 @@
 from server.model import Lectures
 from server import db
 
-def get_all_lectures():
-    pass
+def get_all_lectures(params):
+    # 모든 강의 목록을 내려주자. => 당장은 params 활용 X.
+    return {
+        '임시': '임시'
+    }
